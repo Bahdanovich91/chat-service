56fc75f4392526aa11d9e6b6b36676a16fc41e5f
Bahdanovich91 <113859381+Bahdanovich91@users.noreply.github.com>
2026-02-06 08:12:28 +0300

    Added ActionType, RunChatServerCommand

---
+++ src/Enum/ActionType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum ActionType: string
+{
+    case JoinRoom = 'join_room';
+    case LeaveRoom   = 'leave_room';
+    case SendMessage = 'send_message';
+    case ListRooms = 'list_rooms';
+    case GetRoomHistory = 'get_history';
+}
