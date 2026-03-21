47c537660ab42ae7065f56ee3d55b63a97b1b8bc
Anton Bahdanovich <113859381+Bahdanovich91@users.noreply.github.com>
2026-03-21 13:30:33 +0300

    Small fix for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,12 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    $path = dirname(__DIR__);
+    if (file_exists($path.'/.env')) {
+        (new Dotenv())->bootEnv($path.'/.env');
+    } else {
+        (new Dotenv())->bootEnv($path.'/.env.test');
+    }
 }
 
 if ($_SERVER['APP_DEBUG']) {
